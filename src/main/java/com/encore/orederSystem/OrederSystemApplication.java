cb98e29801e775bae928487bdc39078e50523dee
wlgks7460 <wlgks7460@naver.com>
2024-01-29 02:56:39 +0900

    01-29

---
+++ src/main/java/com/encore/orederSystem/OrederSystemApplication.java
@@ -0,0 +1,13 @@
+package com.encore.orederSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrederSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrederSystemApplication.class, args);
+	}
+
+}
